6db4aa1df514b2b47250c1af4e720256d18af472
Jcbituin <150402651+Jcbituin@users.noreply.github.com>
2023-12-01 09:59:03 +0800

    Create 4_💬_Contact.py

---
+++ page/4_💬_Contact.py
@@ -0,0 +1,35 @@
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+st.title("Contact")
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
+with st.container():
+    st.subheader("Facebook")
+    st.write("https://www.facebook.com/jamecarl.bituin/")
+
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    contact_form = """
+    <form action="https://formsubmit.co/jamescarlbituin017@email.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here" required></textarea>
+     <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
